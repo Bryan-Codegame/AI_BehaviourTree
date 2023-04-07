6289f282bfc4fe096319297443e34c4d4ef0e963
bryanLaptop <bryanesc26@gmail.com>
2023-04-07 16:21:36 -0600

    Add RobberBehaviour.cs Script

---
+++ Assets/BehaviourTree/RobberBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberBehaviour : MonoBehaviour
+{
+    private BehaviourTree tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tree = new BehaviourTree();
+        Node steal = new Node("Steal Something");
+        Node goToDiammond = new Node("Go To Diamond");
+        Node goToVan = new Node("Go To Van");
+        
+        steal.AddChild(goToDiammond);
+        steal.AddChild(goToVan);
+        tree.AddChild(steal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
