3f05750d3aafc5e8d89885b9c2619a861a210f2c
bryanLaptop <bryanesc26@gmail.com>
2023-04-10 13:01:38 -0600

    Selector to choose just one option from the tree nodes

--- Assets/BehaviourTree/Selector.cs
+++ Assets/BehaviourTree/Selector.cs
@@ -12,7 +12,14 @@ public class Selector : Node
     {
         Status childStatus = children[currentChild].Process();
         if (childStatus == Status.RUNNING) return Status.RUNNING;
-        if (childStatus == Status.FAILURE) return childStatus;
+
+        //Since it's a selector, it will select just one option. if the option slected get to Success
+        //the currentChild = 0 to restart the selector and don't exceute both children
+        if (childStatus == Status.SUCCESS)
+        {
+            currentChild = 0;
+            return Status.SUCCESS;
+        }
 
         currentChild++;
         if (currentChild >= children.Count)
